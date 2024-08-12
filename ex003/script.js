949038251d6b3d329f5f2519c18fca779480ac14
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-11 23:15:16 -0300

    method filter

---
+++ ex003/script.js
@@ -0,0 +1,31 @@
+// 3 - Filter (Método de array para filtrar dados)
+
+const arr = [1, 2, 3, 4, 5];//criamos um array, array sempre estão entre colchetes [] e chaves {} definem funções 
+const numerosAltos = arr.filter/*podemos add a variavel . o nome do método*/((n) =>{
+    if(n >= 3){
+        return n;
+    }
+})
+
+console.log(arr);//o array original nao é modificado
+console.log(numerosAltos);//mas a constante numeros altos só ira apresentar os elementos citados dentro do bloco numeros altos. 
+
+//vamos criar um array de objetos
+const usuarios = [  {name: "Matheus", available: true}, 
+                  {name: "Pedro", available: false}, 
+                  {name: "João", available: false}, 
+                  {name: "Roberto", available: true}, 
+                  {name: "Carlos", available: true},
+                 ]
+
+
+//se eu quiser apresentar os usuários que estao disponiveis                 
+const availableUsers = usuarios.filter((user) => user.available);// quando usamos esse código de uma linha só o return ja esta incluso então não é necessário add ele ao código
+
+//se eu quiser apresentar os que não estão disponiveis 
+const notAvailableUsers = usuarios.filter((user) => !user.available);//adicionamos o sinal de negação "!" e solicitamos que seja apresentado no console
+
+console.log(availableUsers);//aqui sera apresentados os que retornaram com o valor true
+console.log(notAvailableUsers);// e aqui os que retornaram com o valor false
+
+
