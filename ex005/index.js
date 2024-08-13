bcf95098ba5525e2ec18718ccfc1ed495ece92d7
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-13 20:18:45 -0300

    templates literals

---
+++ ex005/index.js
@@ -0,0 +1,10 @@
+//template literals
+
+const nomeUsuario = 'Vanessa';//declarei a constante nome do usuario
+const idade = 28;//declarei a variavel idade
+
+console.log(`O nome do usuário é ${nomeUsuario} e ele tem ${idade} anos. `);//template literals forma mais simples
+console.log("O nome do usuário é " + nomeUsuario + " e ele tem " + idade +" anos.")//concantenado 
+
+
+
