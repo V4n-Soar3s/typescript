bddc811cc5f1a1fa304c91375886f2767beeea6a
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-23 23:45:04 -0300

    arrays

---
+++ ex018/index.ts
@@ -0,0 +1,20 @@
+//Arrays sao determinados por colchetes []
+
+//Criando um array numérico 
+let numeros: number[] = [1, 2, 3];//se inserirmos um dado de tipo string vai ser apresentado erro, pois, estamos lidando com um array do tipo number, nao podemos mudar o tipo de dados aqui 
+
+numeros.push(5);//aqui adicionamos mais um valor numérico no array 
+
+console.log(numeros);//apresentamos o array com o numero 5 que adicionamos 
+console.log(numeros[2]); // solicitamos que seja apresentado o numero que esta no indice 2 no console 
+
+//array de texto 
+
+const nomes: string[] = ["Vanessa", "Bruna", "Julia"];
+
+nomes.push("Ana"); //aqui adicionamos o nome ana no array utilizando push 
+
+console.log(nomes); 
+
+
+
