a6cee68931be36e993b856d7698731d9c3ae12a8
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-25 20:19:50 -0300

    funcoes com return

--- ex020/index.js
+++ ex020/index.js
@@ -5,3 +5,16 @@ function soma(a, b) {
     console.log(a + b);
 }
 soma(4, 5); //quando chamamos a função os parametros a serem passados devem corresponder a tipagem definida se inserirmos aqui dois valores que informem strings a função não será executada e o TS informará o erro da nossa sintaxe
+/*
+Tipo de Retorno de Funções
+
+-> Funções geralmente tem o return;
+-> Os retornos também podem ser definidos por nós;
+-> Para isso utilizamos a sintaxe: function myFunction():number{}
+-> Esta função retorna um número;
+
+*/
+function saudacao(name) {
+    return `Olá ${name}`; // solicitei que seja retornado o valor quando invocada a função 
+}
+console.log(saudacao("Vanessa")); // invoquei a funcao e solicitei que seja apresentado um valor 
--- ex020/index.ts
+++ ex020/index.ts
@@ -6,3 +6,19 @@ function soma (a: number, b: number){
 }
 
 soma(4, 5); //quando chamamos a função os parametros a serem passados devem corresponder a tipagem definida se inserirmos aqui dois valores que informem strings a função não será executada e o TS informará o erro da nossa sintaxe
+
+/*
+Tipo de Retorno de Funções 
+
+-> Funções geralmente tem o return;
+-> Os retornos também podem ser definidos por nós;
+-> Para isso utilizamos a sintaxe: function myFunction():number{}
+-> Esta função retorna um número; 
+
+*/
+
+function saudacao(name:string):string{// quando colocamos essa sintaxe bloqueamos o erro de retorno, ele nao aceita o retorno de numero pois nao satisfaz o tipo de string 
+    return `Olá ${name}`// solicitei que seja retornado o valor quando invocada a função 
+}
+
+console.log(saudacao("Vanessa"));// invoquei a funcao e solicitei que seja apresentado um valor 
