1ef299c0f9ff2b534454f9452a57ddfd20ceb339
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-25 20:43:39 -0300

    tipos de objetos

--- ex021/index.js
+++ ex021/index.js
@@ -9,3 +9,10 @@ Tipos de objetos
 -> Os objetos sao derterminados por abertura e fechamento de chaves
 
 */
+//Tipos de objeto
+function passCoordinates(coord) {
+    console.log("X coordinates: " + coord.x); //o objeto coord foi passado entao precisamos acessar ele para acessar a variavel 
+    console.log("Y coordinates: " + coord.y); //o objeto coord foi passado entao precisamos acessar ele para acessar a variavel 
+}
+const objCoord = { x: 329, y: 84.2 };
+passCoordinates(objCoord); //passamos o objCoord porque ele corresponde exatamente com o que a função exige 
--- ex021/index.ts
+++ ex021/index.ts
@@ -9,3 +9,13 @@ Tipos de objetos
 
 */
 
+//Tipos de objeto
+function passCoordinates(coord: {x: number, y:number}){
+    console.log("X coordinates: " + coord.x);//o objeto coord foi passado entao precisamos acessar ele para acessar a variavel 
+    console.log("Y coordinates: " + coord.y); //o objeto coord foi passado entao precisamos acessar ele para acessar a variavel 
+}
+
+const objCoord = {x: 329, y: 84.2};
+
+passCoordinates(objCoord);//passamos o objCoord porque ele corresponde exatamente com o que a função exige 
+
