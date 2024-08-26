2ca0c2bb7caefb34e034969a8b6c7cd94f35c612
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-25 21:46:22 -0300

    validacao de argumento opcional

---
+++ ex022/index.ts
@@ -0,0 +1,25 @@
+/*
+
+Validação de props opcionais 
+
+-> Quando a propriedade é opcional, precisamos criar uma validação;
+-> Isso acontece porque o TS não nos ajuda mais, já que ele deixa de controlar o valor que recebemos;
+-> Para isto utilizamos uma condicional if, e conseguimos resolver a situação; 
+
+*/ 
+
+//validando argumento opcional 
+
+function saudacaoAvancada(primeiroNome: string, sobrenome?: string ){
+    //como validar com if?
+    if(sobrenome !== undefined){
+        return `Olá, ${primeiroNome} ${sobrenome}, tudo bem?`;
+    }
+    //se nao tiver o sobrenome sera apresentada essa mensagem 
+    return `Olá, ${primeiroNome}, tudo bem?`; 
+}
+
+console.log(saudacaoAvancada("Vanessa", "Soares")); //invocamos a funcao 
+console.log(saudacaoAvancada("Vanessa"));// invocamos a funcao sem o sobrenome 
+
+
