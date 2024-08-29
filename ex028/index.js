7ad4b046af4d1f0946deea6cc48e8298be1bff05
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-29 20:21:48 -0300

    Non-null Assertion Operator

--- ex028/index.js
+++ ex028/index.js
@@ -2,4 +2,6 @@
 //Non-null Assertion Operator
 //como selecionar o id que criei no html
 const p = document.getElementById("paragrafo1"); //peguei o elemento que foi criado no HTML e puxei para o TS
-console.log(p.innerHTML); //para mostrar o texto salvo na id "some-p", 
+const p2 = document.getElementById("paragrafo2");
+console.log(p.innerText); //para mostrar o texto salvo na id "paragrafo1"
+console.log(p2.innerText); //para mostrar o texto salvo na id "paragrafo2"
--- ex028/index.ts
+++ ex028/index.ts
@@ -4,4 +4,7 @@
 
 const p = document.getElementById("paragrafo1");//peguei o elemento que foi criado no HTML e puxei para o TS
 
-console.log(p!.innerHTML);//para mostrar o texto salvo na id "some-p", 
+const p2 = document.getElementById("paragrafo2");
+
+console.log(p!.innerText);//para mostrar o texto salvo na id "paragrafo1"
+console.log(p2!.innerText);//para mostrar o texto salvo na id "paragrafo2"
