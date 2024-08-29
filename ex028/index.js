e68b1e764f3c796d250a858ce4c798e2bb985533
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-29 20:16:48 -0300

    Non-null Assertion Operator

---
+++ ex028/index.js
@@ -0,0 +1,5 @@
+"use strict";
+//Non-null Assertion Operator
+//como selecionar o id que criei no html
+const p = document.getElementById("paragrafo1"); //peguei o elemento que foi criado no HTML e puxei para o TS
+console.log(p.innerHTML); //para mostrar o texto salvo na id "some-p", 
