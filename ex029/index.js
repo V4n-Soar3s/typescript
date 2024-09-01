c9a41a0afd0a96e8b6761190818ec82411efedae
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-01 19:46:06 -0300

    bigint

---
+++ ex029/index.js
@@ -0,0 +1,10 @@
+"use strict";
+//Bigint
+//Lembrando que o método Bigint é permitido apenas a partir do es2020
+let n; //declarei uma variavel com o metodo bigint
+//n = 1 nao podemos colocar um numero pequeno dentro de bigint pois o metodo foi criado para armazenamos numeros grandes dentro dele 
+//exemplo:
+n = 1000n;
+console.log(n);
+console.log(typeof n);
+console.log(n + 100n);
