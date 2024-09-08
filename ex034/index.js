b399bb79ea906261339334db9c3d26cb34c082cf
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-08 12:44:54 -0300

    callback sem argumento

---
+++ ex034/index.js
@@ -0,0 +1,16 @@
+"use strict";
+// callback como argumento 
+//criaremos uma função de saudação para ser executada
+function greeting(name) {
+    return `Olá ${name}`;
+}
+//criaremos uma função de pré saudação onde teremos como callback uma função representada por f
+function preGreeting(f, userName) {
+    console.log("Preparando a função!");
+    const greet = f(userName);
+    console.log(greet);
+}
+preGreeting(greeting, "Vanessa"); //estamos chamando a função pre saudação e logo após a função de saudacao
+preGreeting(greeting, "Luiz");
+preGreeting(greeting, "Julia");
+preGreeting(greeting, "Bruna");
