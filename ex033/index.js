3b6f1a8644b434efdbfa5ec8f8edcfc37cf4d2c4
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-08 12:03:48 -0300

    void

---
+++ ex033/index.js
@@ -0,0 +1,16 @@
+"use strict";
+//Funções que nao retornam em nada 
+/**
+ * Podemnos ter funções que não retornam em valores
+ * Qual seria o tipo de dado indicado para essa situação?
+ * Neste caso utilizamos void
+ * Ele vai dizer ao TS que não temos um valor de retorno.
+ * Veja abaixo na pratica:
+ */
+//Void(tipo de retorno para as funções que nao retornam nada)
+function withoutReturn() {
+    console.log("Está função não tem retorno!");
+    return 1; //isso vai dar erro pq nao tem retorno essa é a regra do void
+}
+//vamos executar a função e nao ira retornar nada no navegador
+withoutReturn();
